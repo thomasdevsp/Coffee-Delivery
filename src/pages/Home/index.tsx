1761d82d0900325e76674b58b298cd822425f89c
Thomasdevsp <thomasdevsp@gmail.com>
2024-10-05 15:33:16 -0300

    Introduction finished

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,65 @@
+import {
+  BaseIcon,
+  CoffeeContainerList,
+  ColectionItemContainer,
+  HomeContainer,
+  ImageContainer,
+  IntroductionContainer,
+  ItemContainer,
+  TextContainer,
+} from "./styles";
+import CoffeeCoup from "../../assets/introduction/Imagem.svg";
+import { Coffee, Package, ShoppingCart, Timer } from "@phosphor-icons/react";
+
 export function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <HomeContainer>
+      <IntroductionContainer>
+        <TextContainer>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <h3>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </h3>
+
+          <ColectionItemContainer>
+            <ItemContainer>
+              <BaseIcon statusColor="yellowDark">
+                <ShoppingCart size={16} weight="fill" />
+              </BaseIcon>
+              <span>Compra simples e segura</span>
+            </ItemContainer>
+
+            <ItemContainer>
+              <BaseIcon statusColor="gray">
+                <Package size={16} weight="fill" />
+              </BaseIcon>
+              <span>Embalagem mantém o café intacto</span>
+            </ItemContainer>
+
+            <ItemContainer>
+              <BaseIcon statusColor="yellow">
+                <Timer size={16} weight="fill" />
+              </BaseIcon>
+              <span>Entrega rápida e rastreada</span>
+            </ItemContainer>
+
+            <ItemContainer>
+              <BaseIcon statusColor="purple">
+                <Coffee size={16} weight="fill" />
+              </BaseIcon>
+              <span>O café chega fresquinho até você</span>
+            </ItemContainer>
+          </ColectionItemContainer>
+        </TextContainer>
+        <ImageContainer>
+          <img src={CoffeeCoup} />
+        </ImageContainer>
+      </IntroductionContainer>
+
+      <CoffeeContainerList>
+        <h2>Nossos cafés</h2>
+      </CoffeeContainerList>
+    </HomeContainer>
   );
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,113 @@
+import styled from "styled-components";
+import background from "../../assets/introduction/Background.png";
+
+export const HomeContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const IntroductionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  gap: 3.5rem;
+
+  padding: 6.25rem 10rem;
+  width: 100%;
+
+  background-image: url(${background});
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 1rem;
+  max-width: 38rem;
+
+  h1 {
+    font-family: "Baloo 2", sans-serif;
+    font-weight: bolder;
+    font-size: 3.025rem;
+    color: ${(props) => props.theme["base-title"]};
+    line-height: 130%;
+  }
+
+  h3 {
+    font-family: "Roboto", sans-serif;
+    font-size: 1.275rem;
+    color: ${(props) => props.theme["base-subtitle"]};
+    line-height: 130%;
+  }
+
+  span {
+    font-family: "Roboto", sans-serif;
+    font-size: 1rem;
+    color: ${(props) => props.theme["base-text"]};
+    line-height: 130%;
+  }
+`;
+
+export const ColectionItemContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px;
+
+  margin-top: 4.125rem;
+`;
+
+export const ItemContainer = styled.div`
+  display: flex;
+  align-items: center;
+  max-width: 19rem;
+
+  gap: 12px;
+`;
+
+export const ImageContainer = styled.div`
+  img {
+    max-width: 623px;
+    height: 471px;
+  }
+`;
+
+const STATUS_COLORS = {
+  yellowDark: "yellow-dark",
+  yellow: "yellow",
+  gray: "base-text",
+  purple: "purple",
+} as const;
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS;
+}
+
+export const BaseIcon = styled.div<StatusProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  max-width: 2rem;
+
+  padding: 8px;
+  border-radius: 100%;
+
+  color: ${(props) => props.theme["background"]};
+  background-color: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
+`;
+
+export const CoffeeContainerList = styled.div`
+  width: 100%;
+  padding: 2rem 10rem;
+
+  h2 {
+    font-family: "Baloo 2", sans-serif;
+    font-weight: bolder;
+    font-size: 2rem;
+    color: ${(props) => props.theme["base-subtitle"]};
+    line-height: 130%;
+  }
+`;
